4fd758f82b8ef7ed3262a8c80c18994196e2d062
crhbala <chrisbala8888@gmail.com>
2023-09-29 14:17:58 +0530

    price card completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const Free = (props) => {
+  return(
+    <div className='card' >
+      <span className='card-top'>
+        <h5 className='card-header'>{props.pakage}</h5>
+        <h1>${props.price}/month</h1>
+        <div className='card-header-line'></div>
+      </span>
+      <p><i class="bi bi-check-lg"></i>Single User</p>
+      <p><i class="bi bi-check-lg"></i>50GB Storage</p>
+        <p><i class="bi bi-check-lg"></i>Unlimited Public Project</p>
+        <p><i class="bi bi-check-lg"></i>Community Access</p>
+        <p className='disabled'><i class="bi bi-x"></i>Unlimited Private Project</p>
+        <p className='disabled'><i class="bi bi-x"></i>Dedicated Phone Support</p>
+        <p className='disabled'><i class="bi bi-x"></i>Free Subdomine</p>
+      <p className='disabled'><i class="bi bi-x"></i>Monthly Status Reports</p>
+      <span><button disabled>BUTTON</button></span>
+  </div>
+  )
+}
+const Plus = (props) => {
+  return(
+    <div className='card' >
+      <span className='card-top'>
+        <h5 className='card-header'>{props.pakage}</h5>
+        <h1>${props.price}/month</h1>
+        <div className='card-header-line'></div>
+      </span>
+        <p><i class="bi bi-check-lg"></i>Single User</p>
+        <p><i class="bi bi-check-lg"></i>50GB Storage</p>
+        <p><i class="bi bi-check-lg"></i>Unlimited Public Project</p>
+        <p><i class="bi bi-check-lg"></i>Community Access</p>
+        <p><i class="bi bi-check-lg"></i>Unlimited Private Project</p>
+        <p><i class="bi bi-check-lg"></i>Dedicated Phone Support</p>
+        <p><i class="bi bi-check-lg"></i>Free Subdomine</p>
+      <p className='disabled'><i class="bi bi-x"></i>Monthly Status Reports</p>
+      <span><button disabled>BUTTON</button></span>
+  </div>
+  )
+}
+const Pro = (props) => {
+  
+  return(
+    <div className='card' >
+      <span className='card-top'>
+        <h5 className='card-header'>{props.pakage}</h5>
+        <h1>${props.price}/month</h1>
+        <div className='card-header-line'></div>
+      </span>
+        <p><i class="bi bi-check-lg"></i>Single User</p>
+        <p><i class="bi bi-check-lg"></i>50GB Storage</p>
+        <p><i class="bi bi-check-lg"></i>Unlimited Public Project</p>
+        <p><i class="bi bi-check-lg"></i>Community Access</p>
+        <p><i class="bi bi-check-lg"></i>Unlimited Private Project</p>
+        <p><i class="bi bi-check-lg"></i>Dedicated Phone Support</p>
+        <p><i class="bi bi-check-lg"></i>Free Subdomine</p>
+      <p><i class="bi bi-check-lg"></i>Monthly Status Reports</p>
+      <span><button>BUTTON</button></span>
+  </div>
   )
 }
 
-export default App
+
+const App = () => {
+  
+  return (
+    <div className='container'>
+      <div className='price-card'>
+        <Free pakage = 'FREE' price ='0' />
+        <Plus pakage = 'PLUS' price ='9' />
+        <Pro pakage='PRO' price='49' />
+      </div>
+    </div>
+  )
+  
+}
+
+export default App;
